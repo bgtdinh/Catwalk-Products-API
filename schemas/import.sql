4fd0af3432ea29bca28a16d1690b7a0978ecb0eb
bgtdinh <bgtdinh@gmail.com>
2022-01-20 19:37:42 -0800

    fixing to relative path

--- schemas/import.sql
+++ schemas/import.sql
@@ -1,13 +1,13 @@
 \c productsapi;
 
-copy products(id, product_name, slogan, product_description, category, default_price) from '/home/brian/hackreactor/Project-Catwalk-Products-API/data/product.csv' delimiter ',' csv header;
+copy products(id, product_name, slogan, product_description, category, default_price) from '../data/product.csv' delimiter ',' csv header;
 
-copy styles(id, product_id, style_name, sale_price, original_price, style_default) from '/home/brian/hackreactor/Project-Catwalk-Products-API/data/styles.csv' delimiter ',' csv header;
+copy styles(id, product_id, style_name, sale_price, original_price, style_default) from '../data/styles.csv' delimiter ',' csv header;
 
- copy photos(photo_id, styles_id, normal_url, thumbnail_url) from '/home/brian/hackreactor/Project-Catwalk-Products-API/data/photos.csv' delimiter ',' csv header;
+ copy photos(photo_id, styles_id, normal_url, thumbnail_url) from '../data/photos.csv' delimiter ',' csv header;
 
- copy skus from '/home/brian/hackreactor/Project-Catwalk-Products-API/data/skus.csv' delimiter ',' csv header;
+ copy skus from '../data/skus.csv' delimiter ',' csv header;
 
-  copy related from '/home/brian/hackreactor/Project-Catwalk-Products-API/data/related.csv' delimiter ',' csv header;
+  copy related from '../data/related.csv' delimiter ',' csv header;
 
-  copy features from '/home/brian/hackreactor/Project-Catwalk-Products-API/data/features.csv' delimiter ',' csv header;
+  copy features from '../data/features.csv' delimiter ',' csv header;
